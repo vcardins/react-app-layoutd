aa07dd78527464fcaf2a10c27864c874d36bc8dd
Victor Cardins <victor.cardins@earthdaily.com>
2022-11-02 12:37:03 -0700

    (feat): Added an icon to the demo settings

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -2,6 +2,7 @@ import { render } from 'react-app-layouts';
 
 import { pages, navigation, buildTheme, Providers } from './app';
 import metadata from './manifest.json';
+import { Logo } from './logo';
 
 render({
 	theme: buildTheme(undefined, metadata.theme_color),
@@ -10,6 +11,7 @@ render({
 	pages,
 	version: process?.env?.['VERSION'],
 	Providers,
+	Icon: <Logo size={40} />,
 	settings: {
 		header: { shadowElevation: 1 },
 	},
